583d2cee141b772baf917681a24b5419a3d5d28f
valeroy51 <valeroy51@gmail.com>
2024-08-28 16:19:45 +0700

    yes

--- app/src/main/java/com/example/myapplication5/MainActivity.java
+++ app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -20,7 +20,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 fgfgnvnbvbn
-    private AppBarConfiguration appBarConfiguration;
+    private AppBarConfiguration appBarConfiguration;ggfgf
     private ActivityMainBinding binding;
 
     @Override
